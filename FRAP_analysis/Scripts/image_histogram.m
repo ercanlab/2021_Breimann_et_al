0a05a45a46c98630960fd4a6bca76254f507cf06
LauraBreimann <LauraBreimann@gmail.com>
2021-03-31 19:34:01 +0200

    Added scripts and example data to folders

--- FRAP_analysis/Scripts/image_histogram.m
+++
@@ -1,5 +0,0 @@
-I = imread('200228_EG8899_lif_-_FRAP_006_FRAP_Pre_Series13.tiff');
-imshow(I)
-
-figure;
-imhist(I);
--- FRAP_analysis/Scripts/reg_example.m
+++
@@ -1,7 +0,0 @@
-h = waitbar(0,'please wait...','Name','xy registration...');
-for i = 1:dim3
-    tmp = double(Fim_raw(:,:,i));
-    [output, Greg] = dftregistration(fft2(reg_temp),fft2(tmp));
-    Fim_reg(:,:,i) = abs(ifft2(Greg));
-    waitbar(i/dim3)
-end
