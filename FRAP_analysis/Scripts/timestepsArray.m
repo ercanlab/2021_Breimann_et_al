eaf3089659e78c84512f765fb053bdc47f1de1a0
LauraBreimann <LauraBreimann@gmail.com>
2021-04-09 21:22:51 +0200

    rename folder

--- FRAP_analysis/Scripts/timestepsArray.m
+++
@@ -1,9 +0,0 @@
-function timestepsArray = timestepsArray(dim3_pre, timePre)
-timeAr = [];
-timePre1=timePre;
-    for i = 1:dim3_pre
-        timeAr = [timeAr, timePre];
-        timePre = timePre + timePre1;
-    end    
-    timestepsArray = timeAr;
-end
